257d7e7945d8af9be171c8cc92f72642fb501416
Mike <mikemcguiness@protonmail.com>
2023-08-09 20:59:23 -0700

    Revert "add credentials for Google Cloud"
    This reverts commit e9e3dff51f78cbf4d54d3556bb93cc85c4edbdb8.

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -4,12 +4,6 @@ import tiktoken from 'tiktoken-node';
 import { Storage } from '@google-cloud/storage';
 import Papa from 'papaparse';
 
-const base64Key = process.env.GCLOUD_KEYFILE_CONTENTS_BASE64;
-const jsonString = Buffer.from(base64Key as string, 'base64').toString('utf-8');
-const credentials = JSON.parse(jsonString);
-
-
-
 const openai = new OpenAI();
 
 const queryModel = 'gpt-3.5-turbo'
@@ -74,7 +68,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   async function downloadBlob(bucketName: string, sourceBlobName: string) {
     console.debug("Starting blob download");
 
-    const storage = new Storage({ credentials });
+    const storage = new Storage();
     const bucket = storage.bucket(bucketName);
     const blob = bucket.file(sourceBlobName);
 
