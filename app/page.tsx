b154b91ffeaa38651607878816c8850df2455b12
Mike <mikemcguiness@protonmail.com>
2024-01-05 20:36:47 -0800

    Use lazy loading for client components on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,35 @@
 'use client';
 
-import Head from 'next/head'
+import dynamic from 'next/dynamic';
 
-import CarouselJournalEntries from '@/components/carouselJournalEntries';
-import ChatSample from '@/components/chatSample';
-import CarouselImages from '@/components/carouselImages';
 import Intro from '@/app/intro';
+
+const DynamicCarouselImages = dynamic(() => import('@/components/carouselImages'), {
+  loading: () => <p>Loading...</p>
+});
+
+const DynamicChatSample = dynamic(() => import('@/components/chatSample'), {
+  loading: () => <p>Loading...</p>
+});
+
+const DynamicCarouselJournalEntries = dynamic(() => import('@/components/carouselJournalEntries'), {
+  loading: () => <p>Loading...</p>
+});
 
 export default function Home() {
   return (
-    <>
-      <main role="main">
-        <Intro />
+    <main role="main">
+      <Intro />
 
-        <section className='mt-64 '>
-          <CarouselImages />
-        </section>
-        <section className='mt-64'>
-          <ChatSample />
-        </section>
-        <section className='mt-64 pb-32'>
-          <CarouselJournalEntries />
-        </section>
-      </main>
-    </>
+      <section className='mt-64 '>
+        <DynamicCarouselImages />
+      </section>
+      <section className='mt-64'>
+        <DynamicChatSample />
+      </section>
+      <section className='mt-64 pb-32'>
+        <DynamicCarouselJournalEntries />
+      </section>
+    </main>
   )
 }
