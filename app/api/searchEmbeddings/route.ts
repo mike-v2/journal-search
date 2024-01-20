44c3c426523fa1aaf1bfac1c55341e6ffaf26a9d
Mike <mikemcguiness@protonmail.com>
2024-01-19 18:56:40 -0800

    Convert searchEmbeddings api route to route handler

--- app/api/searchEmbeddings/route.ts
+++ app/api/searchEmbeddings/route.ts
@@ -1,15 +1,11 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest } from 'next/server';
 
 const url = 'https://python-q4npvduerq-uw.a.run.app/';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  try {
-    console.log(req.body);
-    const { query, threshold } = req.body;
+export async function POST(req: NextRequest) {
+  const { query, threshold } = await req.json();
 
+  try {
     const response = await fetch(url, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -20,8 +16,9 @@ export default async function handler(
     });
 
     const data = await response.json();
-    res.status(200).json({ results: data });
+    return Response.json({ results: data });
   } catch (error) {
-    res.status(500).json({ error: error });
+    console.error(error);
+    return Response.json({ error }, { status: 500 });
   }
 }
