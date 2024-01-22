645a803a603a1638ddf1678c76f3b37537ade830
Mike <mikemcguiness@protonmail.com>
2024-01-22 15:01:30 -0800

    Convert createPrismaData api route to route handler

---
+++ app/api/createPrismaData/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import prisma from '@/utils/prisma';
+
+export default async function handler(req: NextRequest) {
+  try {
+    const data = await req.json();
+
+    /* const newEntries = await prisma.journalEntry.createMany({
+        data: data,
+        skipDuplicates: true,
+      }); */
+    /* const newEntry = await prisma.journalEntry.create({
+        data: data,
+      }); */
+    const newTopic = await prisma.journalTopic.create({
+      data: data,
+    });
+    /* const newTopic = await prisma.journalTopic.updateMany({
+        where: {
+          id: data.id,
+        },
+        data: {
+          date: data.date,
+        },
+      }); */
+
+    return NextResponse.json(newTopic);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
