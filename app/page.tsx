7eb76b08e862a0e67e23dc38d449f0c8f3884319
Mike <mikemcguiness@protonmail.com>
2024-01-22 21:26:16 -0800

    Add title header to photograph section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import dynamic from 'next/dynamic';
 
 import Intro from '@/app/intro';
+import Image from 'next/image';
 
 const DynamicCarouselImages = dynamic(
   () => import('@/components/carouselImages'),
@@ -28,6 +29,29 @@ export default function Home() {
       <Intro />
 
       <section className='mt-64 '>
+        <h2 className='relative mx-auto my-12 w-full max-w-lg'>
+          <Image
+            src='/images/banner-photos.png'
+            width={352}
+            height={896}
+            className='relative z-10 h-auto w-full'
+            alt='Photographs title'
+          />
+          <Image
+            src='/images/camera-1.png'
+            width={251}
+            height={206}
+            className='absolute -left-1/2 top-5 z-0 h-auto w-64 opacity-35'
+            alt='old camera'
+          />
+          <Image
+            src='/images/camera-2.png'
+            width={200}
+            height={262}
+            className='absolute -right-1/4 top-1/3 z-0 h-auto w-48 opacity-35'
+            alt='old camera'
+          />
+        </h2>
         <DynamicCarouselImages />
       </section>
       <section className='mt-64'>
