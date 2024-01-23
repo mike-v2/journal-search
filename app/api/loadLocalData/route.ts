b756e4a75e7f1ccf2898883de08581a8a37d2b9d
Mike <mikemcguiness@protonmail.com>
2024-01-22 18:41:25 -0800

    Fix file not found build error

--- app/api/loadLocalData/route.ts
+++ app/api/loadLocalData/route.ts
@@ -3,11 +3,13 @@ import { promises as fs } from 'fs';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
-  const jsonDirectory = path.join(process.cwd(), 'json');
+  return NextResponse.json({ error: 'no file specified' }, { status: 500 });
+
+  /* const jsonDirectory = path.join(process.cwd(), 'json');
   const fileContents = await fs.readFile(
     jsonDirectory + '/1944-analysis.json',
     'utf8',
   );
 
-  return NextResponse.json(fileContents);
+  return NextResponse.json(fileContents); */
 }
