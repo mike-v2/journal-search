a4d8652e5ac54a3c464dff659366a6ea8599df20
Mike <mikemcguiness@protonmail.com>
2024-01-23 17:01:28 -0800

    Add image titles to home page sections

--- app/page.tsx
+++ app/page.tsx
@@ -34,30 +34,48 @@ export default function Home() {
             src='/images/banner-photos.png'
             width={352}
             height={896}
-            className='relative z-10 h-auto w-full'
+            className='h-auto w-full'
             alt='Photographs title'
           />
           <Image
             src='/images/camera-1.png'
             width={251}
             height={206}
-            className='absolute -left-1/2 top-5 z-0 h-auto w-64 opacity-35'
+            className='absolute -left-full top-3/4 z-0 h-auto w-[30rem] opacity-35'
             alt='old camera'
           />
           <Image
             src='/images/camera-2.png'
             width={200}
             height={262}
-            className='absolute -right-1/4 top-1/3 z-0 h-auto w-48 opacity-35'
+            className='absolute -right-2/3 top-[25rem] z-0 h-auto w-72 opacity-35'
             alt='old camera'
           />
         </h2>
         <DynamicCarouselImages />
       </section>
       <section className='mt-64'>
+        <h2 className='mx-auto my-12 w-full max-w-lg'>
+          <Image
+            src='/images/banner-chat.png'
+            width={352}
+            height={896}
+            className='h-auto w-full'
+            alt='Chat title'
+          />
+        </h2>
         <DynamicChatSample />
       </section>
       <section className='mt-64 pb-32'>
+        <h2 className='mx-auto my-12 w-full max-w-lg'>
+          <Image
+            src='/images/banner-sample-entries.png'
+            width={352}
+            height={896}
+            className='h-auto w-full'
+            alt='Sample entries title'
+          />
+        </h2>
         <DynamicCarouselJournalEntries />
       </section>
     </main>
