d557541bccc40015fd14cbbacb02582d4e612c81
Mike McGuiness <mikemcguiness@protonmail.com>
2025-03-04 16:41:16 -0800

    Use present tense in Harry prompt suggestions

--- constants/prompts.ts
+++ constants/prompts.ts
@@ -2,5 +2,5 @@ export const prompts = [
   'What was your favorite road trip?',
   'Tell me about some of your favorite moments with your family.',
   'What was it like to live through WWII?',
-  'Did you have any hobbies?',
+  'Do you have any hobbies?',
 ];
